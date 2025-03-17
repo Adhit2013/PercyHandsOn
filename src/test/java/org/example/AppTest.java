51bbfc0b4b3c599a28d92a0c868e160669233258
Adhit2013 <raj11men@gmail.com>
2025-03-16 22:57:22 -0400

    Updated Percy Test Case Name

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,21 +1,28 @@
 package org.example;
 
 import io.percy.selenium.Percy;
+import org.junit.jupiter.api.TestInfo;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AppTest {
     private static WebDriver driver;
     private static Percy percy;
 
     @Test
-    public void googleTest() {
-        ChromeOptions options = new ChromeOptions();
-        driver = new ChromeDriver(options);
+    public void googleTest(TestInfo testInfo) {
+        Map<String, Object> options = new HashMap<String, Object>();
+        options.put("testCase", testInfo.getDisplayName());
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new ChromeDriver(chromeOptions);
         driver.get("https://google.com");
         percy = new Percy(driver);
-        percy.snapshot("Google Test");
+        percy.snapshot("Google Test", options);
     }
 }
